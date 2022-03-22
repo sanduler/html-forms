8b93920bf5204e7c8d0ed3ceab89facf4207b121
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-21 17:18:27 -0700

    post request

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():  # put application's code here
     return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def login():  # put application's code here
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
